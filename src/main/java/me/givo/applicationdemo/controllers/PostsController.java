4af06207504417b6aa16b7ef779c029f97dbd34f
Jose Alfu <jose@debian-BULLSEYE-live-builder-AMD64>
2021-09-26 23:19:03 -0500

    Error handle implementing

--- src/main/java/me/givo/applicationdemo/controllers/PostsController.java
+++ src/main/java/me/givo/applicationdemo/controllers/PostsController.java
@@ -1,8 +1,11 @@
 package me.givo.applicationdemo.controllers;
 
 
+import me.givo.applicationdemo.models.DirectionIsInvalid;
 import me.givo.applicationdemo.services.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +22,10 @@ public class PostsController {
     public Object getPosts(@RequestParam(name = "sortBy", required = false, defaultValue = "id") String sortBy,
                            @RequestParam(name = "direction", required = false, defaultValue = "asc") String direction,
                            @RequestParam(name = "tags") String... tags) {
+
+        if (direction.compareTo("desc") != 0 && direction.compareTo("asc") != 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DirectionIsInvalid());
+        }
         return postsService.getPostsList(sortBy, direction, tags);
     }
 }
